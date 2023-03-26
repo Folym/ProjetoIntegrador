347378adc46d71424d7108fdd0d2f8669ddf5c1e
Heitor <heitor.folim18@gmail.com>
2023-03-26 17:39:46 -0300

    correcao da dao de jovens e doacao

--- backend/Persistencia/PretendentesDAO.js
+++ backend/Persistencia/PretendentesDAO.js
@@ -1,7 +1,6 @@
 
 
 import Pretendentes from "../Modelo/Pretendentes.js";
-import conectar from "./Conexao.js";
 import Dao from "./Dao.js"
 export default class PretendentesDAO{
     constructor(){}
@@ -9,15 +8,11 @@ export default class PretendentesDAO{
     async  gravar(pret) {
         if(pret instanceof Pretendentes){
             const dao = new Dao();
-            const conexao = await conectar(); // esta aguardanado a conexao  (pret_nome,pret_cpf,pret_cel,pret_email,pret_end,pret_numend,pret_cep,pret_status)
-            conexao.connect();
             const sql = "INSERT INTO Pretendente (pret_nome,pret_cpf,pret_cel,pret_email,pret_end,pret_numend,pret_cep,pret_status)  VALUES ( '"+pret.nome+"','"+pret.cpf+"','"+pret.cel+"','" +pret.email+"','"+pret.end+"',"+pret.numend+","+pret.cep+",'"+pret.status+"');"
             return await dao.gravar(sql);
         }
     }
 
-
-    
     async  atualizar(pret) {
         if(pret instanceof Pretendentes){
             const dao = new Dao();
@@ -38,14 +33,7 @@ export default class PretendentesDAO{
        
         const dao = new Dao();
         const sql = "SELECT * FROM Pretendente WHERE pret_nome like '%"+nome+"%'";
-        //const rows = dao.consultar(sql,dados);
-        // let lista = [];
-        // for(const reg of rows){
-        //     const preten = new Pretendentes(reg['pret_codigo'],reg['pret_nome'],reg['pret_cpf'],
-        //         reg['pret_cel'],reg['pret_email'],reg['pret_end'],reg['pret_numend'],reg['pret_cep'],reg["pret_status"]);
-        //         lista.push(preten);
-        // }
-        return dao.consultar(sql);;
+        return dao.consultar(sql);
         
     }
 
