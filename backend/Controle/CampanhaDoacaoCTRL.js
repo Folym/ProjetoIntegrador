2e3dff95dd1f896c1aa79fcba71845f07e872e28
HenriqueMPelegrini <henriquemp66@gmail.com>
2023-05-12 11:15:18 -0300

    Atualização Imagem Controle

--- backend/Controle/CampanhaDoacaoCTRL.js
+++ backend/Controle/CampanhaDoacaoCTRL.js
@@ -124,4 +124,20 @@ export default class CampanhaDoacaoCTRL{
             });
         }
     }
+
+    gravarImg(req,res){
+        if (req.file) {
+            console.log(req.file);
+            return res.json({
+                erro: false,
+                mensagem: "Upload realizado com sucesso!"
+            });
+        }
+      
+    
+        return res.status(400).json({
+            erro: true,
+            mensagem: "Erro: Upload não realizado com sucesso, necessário enviar uma imagem PNG ou JPG!"
+        });
+    }
 }
--- backend/Rotas/rotaCampanhaDoacao.js
+++ backend/Rotas/rotaCampanhaDoacao.js
@@ -1,5 +1,7 @@
 import {Router} from 'express';
 import CampanhaDoacaoCTRL from '../Controle/CampanhaDoacaoCTRL.js';
+import upload from '../middlewares/uploadImage.js'
+const uploadUser = upload;
 
 const rotaCamp= new Router();
 const ctrlCamp = new CampanhaDoacaoCTRL();
@@ -8,5 +10,7 @@ rotaCamp.get("/:cod", ctrlCamp.consultarCod)
 rotaCamp.post("/", ctrlCamp.gravar)
 rotaCamp.put("/", ctrlCamp.finalizar)
 rotaCamp.delete("/", ctrlCamp.excluir);
+
+rotaCamp.post("/upload-image", uploadUser.single('image'),ctrlCamp.gravarImg)
 
 export default rotaCamp;
