e16ff320838953e0f172e9682dc88e4c8996c20c
Heitor <heitor.folim18@gmail.com>
2023-05-22 00:51:33 -0300

    storeModificação

--- frontend/src/redux/store.js
+++ frontend/src/redux/store.js
@@ -1,20 +1,32 @@
 import { configureStore } from "@reduxjs/toolkit"
+import { configureStore } from "@reduxjs/toolkit"
 import PretendentesSlice from "./redutores/PretendentesSlice";
 import JovensSlice from "./redutores/JovensSlice";
 import DoacaoCProdSlice from "./redutores/DoacaoCProdSlice";
+import DoacaoLProdSlice from "./redutores/DoacaoLProdSlice";
+import DoacaoCDinSlice from "./redutores/DoacaoCDinSlice";
+import DoacaoLDinSlice from "./redutores/DoacaoLDinSlice";
 import FuncionariosSlice from "./redutores/FuncionariosSlice";
 import ProdutoSlice from "./redutores/ProdutoSlice.js";
 import CampanhasSlice from "./redutores/CampDoacaoSlice";
+import DespesasSlice from "./redutores/DespesasSlice";
+import ParcelasSlice from "./redutores/ParcelasSlice.js";
+import TipoDespesaSlice from "./redutores/TipoDespesaSlice.js";
 
 const store = configureStore({
     reducer:{
         pretendentes: PretendentesSlice,
         jovens: JovensSlice,
-        doacao: DoacaoCProdSlice,
+        doacaoCProd: DoacaoCProdSlice,
+        doacaoLProd: DoacaoLProdSlice,
+        doacaoCDin : DoacaoCDinSlice,
+        doacaoLDin : DoacaoLDinSlice,
         funcionarios: FuncionariosSlice,
         produto: ProdutoSlice,
         campanhas : CampanhasSlice,
-
+        despesas: DespesasSlice,
+        parcelas: ParcelasSlice,
+        tiposdesp: TipoDespesaSlice
     }
 
 })
